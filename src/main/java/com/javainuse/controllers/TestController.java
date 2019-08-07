0b4b437c87cf0d9948b1c114c1b64216373afc8b
Deelaka Meegoda <deelakameegoda@gmail.com>
2019-08-07 16:51:40 +0530

    Initial commit

---
+++ src/main/java/com/javainuse/controllers/TestController.java
@@ -0,0 +1,35 @@
+package com.javainuse.controllers;
+
+import com.javainuse.model.Auth;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.javainuse.model.Employee;
+
+@RestController
+public class TestController {
+
+    @RequestMapping(value = "/employee", method = RequestMethod.GET)
+    public Employee firstPage() {
+
+        Employee emp = new Employee();
+        emp.setName("emp1");
+        emp.setDesignation("manager");
+        emp.setEmpId("1");
+        emp.setSalary(3000);
+
+        return emp;
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public Auth secondPage() {
+
+        Auth login = new Auth();
+        login.setUsername("Deelaka");
+        login.setPassword("ABCD");
+
+        return login;
+    }
+
+}
